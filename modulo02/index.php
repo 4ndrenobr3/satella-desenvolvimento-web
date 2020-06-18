d97abf3780f13a6d9959c7c4711a8317a39db5c5
4ndrenobr3 <nobre4ndr3@gmail.com>
2020-06-18 19:58:59 -0300

    Estilizado o formulário.

--- modulo02/index.php
+++ modulo02/index.php
@@ -9,27 +9,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/js/script.js">
     <link rel="stylesheet" href="./assets/css/unsemantic-grid-responsive.css">
     <title>Contato</title>
 </head>
 <body>
     <section>
         <div class="container">
-            <form action="" method="post">
-                <div>
+            <form action="" method="post" id="formContact">
+                <div class="grid-50 mobile-grid-100">
                     <label for="name">Nome</label>
-                    <input type="text" name="name">
+                    <input type="text" name="name" id="name">
                 </div>
-                <div>
+                <div class="grid-50 mobile-grid-100">
                     <label for="email">E-mail</label>
-                    <input type="email" name="email">
+                    <input type="email" name="email" id="email">
                 </div>
-                <div>
+                <div class="grid-50 mobile-grid-100">
                     <label for="phone">Telefone</label>
-                    <input type="number" name="phone">
+                    <input type="tel" name="phone" id="phone">
                 </div>
-                <div>
+                <div class="grid-50 mobile-grid-100">
                     <label for="subject">Assunto</label>
                     <select name="subject" id="subject">
                         <option value="1">Comercial</option>
@@ -38,12 +37,16 @@
                         <option value="4">Outros</option>
                     </select>
                 </div>
-                <div>
+                <div class="grid-100 mobile-grid-100">
                     <label for="message">Menssagem</label>
                     <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+                <div class="grid-30 mobile-grid-100">
+                    <button type="submit">Enviar</button>
                 </div>
             </form>
         </div>
     </section>
+    <script src="./assets/js/script.js"></script>
 </body>
 </html>
